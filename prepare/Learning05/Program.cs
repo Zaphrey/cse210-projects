20fc34b84dc5ada9068edfec41e594bfe27131e7
Zachary Humphreys <zachary.humphreys32@gmail.com>
2024-02-07 11:38:07 -0500

    Completed learning05 activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>() {
+            new Square("Red", 10),
+            new Rectangle("Blue", 10, 5),
+            new Circle("Green", 30)
+        };
+
+        foreach (Shape s in shapes)
+        {
+            Type shapeType = s.GetType();
+            Console.WriteLine($"The color of the {shapeType} is {s.GetColor()}, and the area is {s.GetArea()}");
+        }
     }
 }
